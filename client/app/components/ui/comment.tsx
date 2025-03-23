5e1af43aa6110ad5b49b5b464d9200cecf9db833
varphi-online <nbash1984@gmail.com>
2025-03-23 08:03:33 -0700

    fixed comment bug

--- client/app/components/ui/comment.tsx
+++ client/app/components/ui/comment.tsx
@@ -6,7 +6,21 @@ export default function Comment({ comment }: { comment: CommentData }) {
   return (
     <div className="border-secondary-foreground bg-secondary text-secondary-foreground flex items-start gap-3 rounded-3xl border p-4">
       <Link to={"/" + comment.username}>
-        <img src={"/Amber-1705-cropped.jpg"} className="size-10 rounded-full" />
+      <div
+        className={`flex min-w-10 min-h-10 items-center justify-center overflow-hidden rounded-full mt-4 ${
+          comment.profile_picture
+            ? ""
+            : "border-muted-foreground border-1 border-solid"
+        }`}
+      >
+        {comment.profile_picture ? (
+          <img src={comment.profile_picture} className="object-cover size-10" />
+        ) : (
+          <p className="text-secondary-foreground text-base font-light">
+            {(comment.display_name || "   ").substring(0, 2).toUpperCase()}
+          </p>
+        )}
+      </div>
       </Link>
       <div className="flex w-full flex-col">
         <Link to={"/" + comment.username}>
