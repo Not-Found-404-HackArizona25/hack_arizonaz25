7e8472e03ab80a71f1e313d16311916e78487a40
varphi-online <nbash1984@gmail.com>
2025-03-22 22:23:51 -0700

    created search:GET to search by supers

--- client/app/routes/dashboard.tsx
+++ client/app/routes/dashboard.tsx
@@ -2,19 +2,7 @@ import { apiFetch } from "@/lib/utils";
 import type { Route } from "./+types/dashboard";
 
 export async function clientLoader({}: Route.ClientLoaderArgs) {
-  const response1 = await apiFetch("/super", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      name: 'HackPath',
-      leader: 2,
-      description: 'Path to hack!',
-      type: 'club',
-    })
-  })
-  const test = await response1.json();
-  console.log(test.data)
-  const response = await apiFetch("/super/43", {
+  const response = await apiFetch("/super?type=club&search=enth", {
       method: "GET",
       headers: { "Content-Type": "application/json" },
     });
