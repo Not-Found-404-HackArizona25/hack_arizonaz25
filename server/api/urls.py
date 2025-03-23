b0ebcde9a1d9ceeb465b10b7e70c616912b9f070
BaoNguyen09 <nttbvlogs@gmail.com>
2025-03-22 20:11:03 -0700

    feat: add a path to get multiple posts

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserRegistrationView, SessionView, CurrentUserView
+from .views import UserRegistrationView, SessionView, CurrentUserView, PostView
 
 urlpatterns = [
     path('users', UserRegistrationView.as_view(), name='user-register'),
     path('users/me', CurrentUserView.as_view(), name='current-user'),
     path('sessions', SessionView.as_view(), name='sessions'),
+    path('posts', PostView.as_view(), name='posts')
 ]
