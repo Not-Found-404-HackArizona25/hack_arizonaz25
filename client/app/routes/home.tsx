48220c608a22187dc86e85e8d43317311c713f06
jjj404 <jjaeger4@arizona.edu>
2025-03-23 02:03:25 -0700

    added the feed functionality to home

--- client/app/routes/home.tsx
+++ client/app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { apiFetch } from "@/lib/utils";
 import { Link } from "react-router";
 import type { Route } from "./+types/home";
 
@@ -8,6 +9,62 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <div><Link to="/login">Login</Link><br/><Link to="/login">Register</Link></div>;
+export async function clientLoader({ params }: Route.ClientLoaderArgs) {
+  const response =
+    await apiFetch('/posts', {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+  if (response.ok){
+    const json: { data: { posts: PostData[] } } = await response.json();
+    return json.data.posts;
+  }
+}
+
+export default function Home({ loaderData }): Route.ComponentProps {
+  const createPost = async () => {
+    const response = await apiFetch('/posts', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: "test title",
+        text: "WOW post away",
+        contentType: "TEXT",
+        misc: 56789376592,
+      })
+    })
+    if (response.ok){
+        const json: { data: PostData[] } = await response.json();
+      loaderData.push(json.data);
+    }
+  }
+  
+  createPost();
+  const postData = loaderData || [];
+  
+  return (
+    <div>
+      <Link to="/login">
+        Login
+      </Link>
+      <br/>
+      <Link to="/register">
+        Register
+      </Link>
+      <h2>Posts</h2>
+      {postData.length > 0 ? (
+        <div className="posts-list">
+          {postData.map((post: any) => (
+            <div key={post.id} className="post-card">
+              <p>{post.username}</p>
+              <h3>{post.title}</h3>
+              <p>{post.text}</p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>No posts yet.</p>
+      )}
+    </div>
+  );
 }
