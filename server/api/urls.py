d6ae16e7a30ec9bf38784eb53752b1c77c874d2c
varphi-online <nbash1984@gmail.com>
2025-03-22 21:28:32 -0700

    added super by ID

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 
-from .views import UserRegistrationView, SessionView, CurrentUserView, SuperView, UserIDView
+from .views import UserRegistrationView, SessionView, CurrentUserView, SuperView, UserIDView, SuperIDView
 
 urlpatterns = [
     path('users', UserRegistrationView.as_view(), name='user-register'),
     path('users/me', CurrentUserView.as_view(), name='current-user'),
     path('sessions', SessionView.as_view(), name='sessions'),
     path('users/<int:user_id>', UserIDView.as_view(), name='user-detail'),
-    path('super',SuperView.as_view(), name='make edit super')
+    path('super',SuperView.as_view(), name='make edit super'),
+    path('super/<int:super_id>', SuperIDView.as_view(),name='super-detail')
 ]
